ea4a3fcac7095768c2ac8a8d1f7cedec86996a10
CORCUFF Johann <johann.corcuff@gmail.com>
2018-12-05 00:26:08 +0100

    Instantiation of the 3 different types of WebMediaPlayers : Video | Audio slideshow | slideshow

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@ import WebMediaPlayer from './WebMediaPlayer/WebMediaPlayer'
 
 class App extends Component {
   render() {
-    /*const imageSequence = [
-      {
-          src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide1.jpg",
-          endTime: 2.0
-      }, {
-          src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide2.jpg",
-          endTime: 4.0
-      }, {
-          src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide3.jpg",
-          endTime: 6.0
-      }, {
-          src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide4.jpg",
-          endTime: 8.0
-      }, {
-          src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide5.jpg",
-          endTime: 120.0
-      }];
-      const imasequenceJSON  = JSON.stringify(imageSequence);*/
 
-    const imasequence = "Hello biatch!";
 
     return (
       <div className="App">
@@ -31,9 +12,53 @@ class App extends Component {
         <header className="App-header">
           <h1>React-WebMediaPlayer</h1>
         </header>
-        <WebMediaPlayer thumbnail="img/bbb.jpg" video="https://nusid.net/video.mp4"/>
-        <WebMediaPlayer thumbnail="https://i.ytimg.com/vi_webp/qIM0kucdiGM/sddefault.webp" imageSequence="{imasequence}"/>
-        <WebMediaPlayer thumbnail="https://i.ytimg.com/vi_webp/qIM0kucdiGM/sddefault.webp" audio="https://nusid.net/audio.flac" imageSequence="{ imasequence }"/>
+        <WebMediaPlayer 
+          thumbnail="img/bbb.jpg" 
+          video="https://nusid.net/video.mp4"
+          width="560"
+          height="315"
+        />
+
+        <WebMediaPlayer 
+          thumbnail="https://i.ytimg.com/vi_webp/qIM0kucdiGM/sddefault.webp" 
+          imageSequence={
+          [{
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide1.jpg",
+            endTime: 2.0
+          }, {
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide2.jpg",
+            endTime: 4.0
+          }, {
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide3.jpg",
+            endTime: 6.0
+          }, {
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide4.jpg",
+            endTime: 8.0
+          }, {
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide5.jpg",
+            endTime: 120.0
+          }]}
+        />
+        
+        <WebMediaPlayer 
+          thumbnail="https://i.ytimg.com/vi_webp/qIM0kucdiGM/sddefault.webp" audio="https://nusid.net/audio.flac" imageSequence={
+          [{
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide1.jpg",
+            endTime: 2.0
+          },{
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide2.jpg",
+            endTime: 4.0
+          },{
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide3.jpg",
+            endTime: 6.0
+          },{
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide4.jpg",
+            endTime: 8.0
+          },{
+            src: "https://slideshowexemple1-djlttrqvkb.now.sh/slide5.jpg",
+            endTime: 120.0
+          }]}
+        />
       </div>
     );
   }
