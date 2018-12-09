8505ff8f4e6e4da60e1b60bf66ff699d7b9b59cb
CORCUFF Johann <johann.corcuff@gmail.com>
2018-12-09 14:27:41 +0100

    Development - Functional state initiator class & Webmediaplayer reducer able to perform actions

--- src/WebMediaPlayer/WebMediaPlayer.js
+++ src/WebMediaPlayer/WebMediaPlayer.js
@@ -1,104 +1,36 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { getInitState } from './service/StateInitiator'
 import './WebMediaPlayer.css';
-
-const DEFAULT_WIDTH = 560 //560;
-const DEFAULT_HEIGHT = 315 //315;
-const DEFAULT_ALLOW_FULL_FRAME = true;
-const DEFAULT_VOLUME = 1.0;
 
 class WebMediaPlayer extends Component {
 
   constructor(options) {
     super(options);
-    if (options === undefined){
-      throw new Error("No options given to the player !");
-    }
-    var state = {};
-
-    if (options.hasOwnProperty("video")){
-      if (options.hasOwnProperty("audio")){
-        throw new Error("Combination impossible");
-      } else if (options.hasOwnProperty("imageSequence")){
-        throw new Error("Combination impossible");
-      }  else {
-        state = this.initVideoPlayerState(options);
-      }
-    } else if (options.hasOwnProperty("slideshow")){
-      if (options.hasOwnProperty("audio")){
-        state = this.initAudioSlideshowPlayerState(options);
-      } else if (options.hasOwnProperty("video")){
-        throw new Error("Combination impossible");
-      }  else {
-        state = this.initSlideshowPlayerState(options);
-      }
-    } else {
-      throw new Error("Combination impossible");
-    }
-
-    state.thumbnail= options.thumbnail;
-    state.title= options.title;
-    
-    if (options.hasOwnProperty("height")){
-      state.height = options.height;
-    } else {
-      state.height = DEFAULT_HEIGHT;
-    }
-    if (options.hasOwnProperty("width")){
-      state.width = options.width;
-    } else {
-      state.width = DEFAULT_WIDTH;
-    }
-    if (options.hasOwnProperty("volume")){
-      state.volume = options.volume;
-    } else {
-      state.volume = DEFAULT_VOLUME;
-    }
-    if (options.hasOwnProperty("allowFullFrame")){
-      state.allowFullFrame = options.allowFullFrame;
-    } else {
-      state.allowFullFrame = DEFAULT_ALLOW_FULL_FRAME;
-    }
-
-    this.store = createStore(() => state);
+    this.store = createStore(this.reducer);
   };
 
-  initSlideshowPlayerState(options){
-    let state = {};
-    state.hasAudio = false;
-    state.hasSlideshow = true;
-    state.hasVideo = false;
-    if(options.slideshow.slice(-1)[0].endTime === undefined){
-      throw new Error("No time specified for slideshow");
+  reducer = (state, action) => {
+    if(state === undefined){
+      state =  getInitState(this.props);
     }
-    state.timeLength = options.slideshow.slice(-1)[0].endTime;
-    state.slideshow = options.slideshow;
-    return state;
-  }
-
-  initAudioSlideshowPlayerState(options){
-    let state = {};
-    state.hasAudio = true;
-    state.hasSlideshow = true;
-    state.hasVideo = false;
-    if(options.slideshow.slice(-1)[0].endTime === undefined){
-      throw new Error("No time specified for slideshow");
+    if(action === undefined){
+      return state;
     }
-    state.timeLength = options.slideshow.slice(-1)[0].endTime;
-    state.slideshow = options.slideshow;
-    state.audio = options.audio;
-    return state;
-  }
-
-  initVideoPlayerState(options){
-    let state = {};
-    state.hasVideo = true;
-    state.hasAudio = false;
-    state.hasSlideshow = false;
-    state.video = options.video;
-    return state;
-  }
+    switch(action.type) {
+      case 'INCREMENT':
+        return {
+          count: state.count + 1
+        };
+      case 'DECREMENT':
+        return {
+          count: state.count - 1
+        };
+      default:
+        return state;
+    }
+  };
 
   render() {
     console.log(this.store.getState())
---
+++ src/WebMediaPlayer/service/StateInitiator.js
@@ -0,0 +1,92 @@
+const DEFAULT_WIDTH = 560 //560;
+const DEFAULT_HEIGHT = 315 //315;
+const DEFAULT_ALLOW_FULL_FRAME = true;
+const DEFAULT_VOLUME = 1.0;
+
+const initSlideshowPlayerState = (options) => {
+    let state = {};
+    state.hasAudio = false;
+    state.hasSlideshow = true;
+    state.hasVideo = false;
+    if (options.slideshow.slice(-1)[0].endTime === undefined) {
+        throw new Error("No time specified for slideshow");
+    }
+    state.timeLength = options.slideshow.slice(-1)[0].endTime;
+    state.slideshow = options.slideshow;
+    return state;
+}
+
+const initAudioSlideshowPlayerState = (options) => {
+    let state = {};
+    state.hasAudio = true;
+    state.hasSlideshow = true;
+    state.hasVideo = false;
+    if (options.slideshow.slice(-1)[0].endTime === undefined) {
+        throw new Error("No time specified for slideshow");
+    }
+    state.timeLength = options.slideshow.slice(-1)[0].endTime;
+    state.slideshow = options.slideshow;
+    state.audio = options.audio;
+    return state;
+}
+
+const initVideoPlayerState = (options) => {
+    let state = {};
+    state.hasVideo = true;
+    state.hasAudio = false;
+    state.hasSlideshow = false;
+    state.video = options.video;
+    return state;
+}
+
+const getInitState = (options) => {
+    if (options === undefined) {
+        throw new Error("No options given to the player !");
+    }
+    let state = {};
+    if (options.hasOwnProperty("video")) {
+        if (options.hasOwnProperty("audio")) {
+            throw new Error("Combination impossible");
+        } else if (options.hasOwnProperty("imageSequence")) {
+            throw new Error("Combination impossible");
+        } else {
+            state = initVideoPlayerState(options);
+        }
+    } else if (options.hasOwnProperty("slideshow")) {
+        if (options.hasOwnProperty("audio")) {
+            state = initAudioSlideshowPlayerState(options);
+        } else if (options.hasOwnProperty("video")) {
+            throw new Error("Combination impossible");
+        } else {
+            state = initSlideshowPlayerState(options);
+        }
+    } else {
+        throw new Error("Combination impossible");
+    }
+    state.thumbnail = options.thumbnail;
+    state.title = options.title;
+
+    if (options.hasOwnProperty("height")) {
+        state.height = options.height;
+    } else {
+        state.height = DEFAULT_HEIGHT;
+    }
+    if (options.hasOwnProperty("width")) {
+        state.width = options.width;
+    } else {
+        state.width = DEFAULT_WIDTH;
+    }
+    if (options.hasOwnProperty("volume")) {
+        state.volume = options.volume;
+    } else {
+        state.volume = DEFAULT_VOLUME;
+    }
+    if (options.hasOwnProperty("allowFullFrame")) {
+        state.allowFullFrame = options.allowFullFrame;
+    } else {
+        state.allowFullFrame = DEFAULT_ALLOW_FULL_FRAME;
+    }
+    return state;
+}
+
+export { getInitState }
