9739942391eb9aed0d4aaa6135e33a420a209fd7
JohannC <johann.corcuff@gmail.com>
2019-03-01 20:55:02 +0100

    Development - Previous & Next events

--- src/WebMediaPlayer/components/MenuBar/NextButton.js
+++ src/WebMediaPlayer/components/MenuBar/NextButton.js
@@ -3,18 +3,22 @@ import { connect } from 'react-redux';
 import './Button.css';
 
 class NextButton extends Component {
-    render = () => {
-        return (
-            <div className="wmp-tool-button material-icons light-grey-to-white md-26">
-                skip_next
-            </div>    
-        );
-    }
+  handleClick = () => {
+    this.props.dispatch({ type: 'ASK_NEXT' });
+  }
+  
+  render = () => {
+    return (
+      <div className="wmp-tool-button material-icons light-grey-to-white md-26">
+        skip_next onClick={this.handleClick}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-    };
+  return {
+  };
 };
 
 export default connect(mapStateToProps)(NextButton);
--- src/WebMediaPlayer/components/MenuBar/PreviousButton.js
+++ src/WebMediaPlayer/components/MenuBar/PreviousButton.js
@@ -3,18 +3,22 @@ import { connect } from 'react-redux';
 import './Button.css';
 
 class PreviousButton extends Component {
-    render = () => {
-        return (
-            <div className="wmp-tool-button material-icons light-grey-to-white md-26">
-                skip_previous
-            </div>    
-        );
-    }
+  handleClick = () => {
+    this.props.dispatch({ type: 'ASK_PREVIOUS' });
+  }
+  
+  render = () => {
+    return (
+      <div className="wmp-tool-button material-icons light-grey-to-white md-26" onClick={this.handleClick}>
+        skip_previous
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-    };
+  return {
+  };
 };
 
 export default connect(mapStateToProps)(PreviousButton);
