9f0ca3be7b2aa0b2ca135ad0f07feec0c8c6fc45
JohannC <johann.corcuff@gmail.com>
2019-06-02 22:55:16 +0200

    Test - Improving Container.js integration test

--- tests/integration/components/Container.int.test.js
+++ tests/integration/components/Container.int.test.js
@@ -3,18 +3,29 @@ import Enzyme, { mount } from 'enzyme';
 import createTestStore from '../Utils';
 import Adapter from 'enzyme-adapter-react-16';
 import Container from '../../../src/components/Container';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 Enzyme.configure({ adapter: new Adapter() });
 
+
+
 describe('Integration tests - Container', () => {
+
     let store;
     let dispatchSpy;
+    let await4Seconds = () => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, 4000);
+        });
+    }
 
     beforeEach(() => {
         ({ store, dispatchSpy } = createTestStore());
     });
 
-    it('Container event listeners', () => {
+
+    it('Container event listeners', (done) => {
         const initState = {
             timeLastUserAction: undefined,
             width: 500,
@@ -34,7 +45,7 @@ describe('Integration tests - Container', () => {
             style: false,
             id: "id-player",
             isTestEnvironment: false,
-            volume:1
+            volume: 1
         }
 
         store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
@@ -44,7 +55,7 @@ describe('Integration tests - Container', () => {
                 <Container />
             </Provider>
         );
-        
+
         container.simulate("mouseenter");
         expect(dispatchSpy).toHaveBeenCalledWith({ type: "HIGHLIGHT_PLAYER" });
 
@@ -54,15 +65,29 @@ describe('Integration tests - Container', () => {
         container.simulate("click");
         expect(dispatchSpy).toHaveBeenCalledWith({ type: "INITIALIZE_PLAYER" });
         expect(dispatchSpy).toHaveBeenCalledWith({ type: "PLAY" });
-        
-        container.simulate("mouseenter");        
+
+        container.simulate("mouseenter");
         expect(dispatchSpy).not.toHaveBeenLastCalledWith({ type: "HIGHLIGHT_PLAYER" });
-        
-        container.simulate("mousemove");        
+
+        container.simulate("mousemove");
         expect(dispatchSpy).toHaveBeenCalledWith({ type: "USER_ACTIVE" });
 
         container.simulate("mouseleave");
         expect(dispatchSpy).toHaveBeenCalledWith({ type: "HIDE_MENUS" });
+
+        container.simulate("click");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "PREVENT_MENU_HIDING" });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "PAUSE" });
+
+        container.simulate("click");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "ALLOW_MENU_HIDING" });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "PLAY" });
+        done();
+        await4Seconds().then(() => {
+            expect(dispatchSpy).toHaveBeenLastCalledWith({ type: "HIDE_MENUS" });
+            done();
+        });
+
     });
 
 });
