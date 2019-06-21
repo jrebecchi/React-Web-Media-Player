0c5a19aa7dd9674a85974c26ec419cf0920a4d27
JohannC <johann.corcuff@gmail.com>
2019-06-21 19:51:22 +0200

    Test - Mixer integration test - Playing Slideshownode c:/Users/JRI1/Documents/dev/React-Web-Media-Player/node_modules/jest/bin/jest.js c:/Users/JRI1/Documents/dev/React-Web-Media-Player/tests/integration/components/Medias/Mixer.int.test.js

--- tests/integration/components/Medias/Mixer.int.test.js
+++ tests/integration/components/Medias/Mixer.int.test.js
@@ -56,14 +56,14 @@ describe('Integration tests - Mixer', () => {
         );
         //Initialize player & load Video
         store.dispatch({ type: 'INITIALIZE_PLAYER' });
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: "LOADING"});
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "LOADING" });
         jest.clearAllMocks();
-        
+
         //Start playing 
-        store.dispatch({ type: 'UPDATE_DURATION', payload: { duration: 596.427756 }  });
+        store.dispatch({ type: 'UPDATE_DURATION', payload: { duration: 596.427756 } });
         store.dispatch({ type: 'UPDATE_VIDEO_WIDTH', payload: { videoWidth: 500 } });
         store.dispatch({ type: 'UPDATE_VIDEO_HEIGHT', payload: { videoHeight: 200 } });
-        
+
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_CURRENT_TIME', payload: expect.anything() });
         jest.clearAllMocks();
 
@@ -72,17 +72,17 @@ describe('Integration tests - Mixer', () => {
 
         //Change time
         store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 110 } });
-        
+
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_CURRENT_TIME', payload: { currentTime: 110 } });
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: "isTreated" } });
         jest.clearAllMocks();
-        
+
         //Terminate reading
         store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 596.427756 } });
-        
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'READING_TERMINATED'});
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'PAUSE'});
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SHOW_MENUS'});
+
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'READING_TERMINATED' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'PAUSE' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SHOW_MENUS' });
         jest.clearAllMocks();
 
     });
@@ -113,7 +113,7 @@ describe('Integration tests - Mixer', () => {
             autoplay: false,
             muted: false,
             isFullscreenActivated: false,
-            video: "https://nusid.net/audio.mp3",
+            audio: "https://nusid.net/audio.mp3",
             width: 560,
             height: 315,
         }
@@ -128,12 +128,12 @@ describe('Integration tests - Mixer', () => {
         );
         //Initialize player & load Video
         store.dispatch({ type: 'INITIALIZE_PLAYER' });
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: "LOADING"});
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "LOADING" });
         jest.clearAllMocks();
-        
+
         //Start playing 
-        store.dispatch({ type: 'UPDATE_DURATION', payload: { duration: 596.427756 }  });
-        
+        store.dispatch({ type: 'UPDATE_DURATION', payload: { duration: 596.427756 } });
+
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_CURRENT_TIME', payload: expect.anything() });
         jest.clearAllMocks();
 
@@ -142,17 +142,194 @@ describe('Integration tests - Mixer', () => {
 
         //Change time
         store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 110 } });
-        
+
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_CURRENT_TIME', payload: { currentTime: 110 } });
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: "isTreated" } });
         jest.clearAllMocks();
-        
+
         //Terminate reading
         store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 596.427756 } });
-        
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'READING_TERMINATED'});
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'PAUSE'});
-        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SHOW_MENUS'});
+
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'READING_TERMINATED' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'PAUSE' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SHOW_MENUS' });
+        jest.clearAllMocks();
+
+    });
+
+    it('Mixer - Slideshow', () => {
+
+        const initState = {
+            channelsWait: false,
+            timeRangeBuffered: 0,
+            askNextImage: null,
+            askPreviousImage: null,
+            volume: 1,
+            hasVideo: false,
+            isVideoReady: false,
+            hasAudio: false,
+            isAudioReady: false,
+            hasSlideshow: true,
+            isSlideshowReady: false,
+            isInitialized: false,
+            currentTime: 0,
+            askedTime: null,
+            isPlaying: false,
+            duration: 28,
+            hasVinyl: true,
+            isVinylReady: false,
+            isReadingTerminated: false,
+            allowUnhighlightProgressBar: true,
+            autoplay: false,
+            muted: false,
+            isFullscreenActivated: false,
+            imageDisplayed:{
+                element: {
+                    width: "50px",
+                    height: "50px"
+                }
+            },
+            width: 560,
+            height: 315,
+            slideshow: [{
+                img: "https://nusid.net/slide1.jpg",
+                endTime: 4.0
+            }, {
+                img: "https://nusid.net/slide2.jpg",
+                endTime: 8.0
+            }, {
+                img: "https://nusid.net/slide3.jpg",
+                endTime: 12.0
+            }, {
+                img: "https://nusid.net/slide4.jpg",
+                endTime: 16.0
+            }, {
+                img: "https://nusid.net/slide5.jpg",
+                endTime: 20.0
+            }, {
+                img: "https://nusid.net/slide6.jpg",
+                endTime: 24.0
+            }, {
+                img: "https://nusid.net/slide7.jpg",
+                endTime: 28.0
+            }]
+        }
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+
+        const progressBar = mount(
+            <Provider store={store}>
+                <Mixer />
+            </Provider>
+        );
+        //Initialize player & load Video
+        store.dispatch({ type: 'INITIALIZE_PLAYER' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "LOADING" });
+        jest.clearAllMocks();
+
+        //Change time
+        store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 110 } });
+
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_CURRENT_TIME', payload: { currentTime: 110 } });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: "isTreated" } });
+        jest.clearAllMocks();
+
+        //Terminate reading
+        store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 596.427756 } });
+
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'READING_TERMINATED' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'PAUSE' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SHOW_MENUS' });
+        jest.clearAllMocks();
+
+    });
+
+    it('Mixer - Slideshow', () => {
+
+        const initState = {
+            channelsWait: false,
+            timeRangeBuffered: 0,
+            askNextImage: null,
+            askPreviousImage: null,
+            volume: 1,
+            hasVideo: false,
+            isVideoReady: false,
+            hasAudio: true,
+            isAudioReady: false,
+            hasSlideshow: true,
+            isSlideshowReady: false,
+            isInitialized: false,
+            currentTime: 0,
+            askedTime: null,
+            isPlaying: false,
+            duration: 28,
+            hasVinyl: true,
+            isVinylReady: false,
+            isReadingTerminated: false,
+            allowUnhighlightProgressBar: true,
+            autoplay: false,
+            muted: false,
+            isFullscreenActivated: false,
+            imageDisplayed:{
+                element: {
+                    width: "50px",
+                    height: "50px"
+                }
+            },
+            width: 560,
+            height: 315,
+            slideshow: [{
+                img: "https://nusid.net/slide1.jpg",
+                endTime: 4.0
+            }, {
+                img: "https://nusid.net/slide2.jpg",
+                endTime: 8.0
+            }, {
+                img: "https://nusid.net/slide3.jpg",
+                endTime: 12.0
+            }, {
+                img: "https://nusid.net/slide4.jpg",
+                endTime: 16.0
+            }, {
+                img: "https://nusid.net/slide5.jpg",
+                endTime: 20.0
+            }, {
+                img: "https://nusid.net/slide6.jpg",
+                endTime: 24.0
+            }, {
+                img: "https://nusid.net/slide7.jpg",
+                endTime: 28.0
+            }],
+            audio: "https://nusid.net/audio.mp3"
+        }
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+
+        const progressBar = mount(
+            <Provider store={store}>
+                <Mixer />
+            </Provider>
+        );
+        //Initialize player & load Video
+        store.dispatch({ type: 'INITIALIZE_PLAYER' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: "LOADING" });
+        jest.clearAllMocks();
+
+        //Change time
+        store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 110 } });
+
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_CURRENT_TIME', payload: { currentTime: 110 } });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: "isTreated" } });
+        jest.clearAllMocks();
+
+        //Terminate reading
+        store.dispatch({ type: 'UPDATE_ASKED_TIME', payload: { askedTime: 596.427756 } });
+
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'READING_TERMINATED' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'PAUSE' });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SHOW_MENUS' });
         jest.clearAllMocks();
 
     });
