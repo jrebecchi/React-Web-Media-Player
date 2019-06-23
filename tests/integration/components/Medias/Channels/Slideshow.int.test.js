36977e1523deeeb34e050f965805226fda3851db
JohannC <johann.corcuff@gmail.com>
2019-06-23 23:33:07 +0200

    Test - Slideshow new integration tests

--- tests/integration/components/Medias/Channels/Slideshow.int.test.js
+++ tests/integration/components/Medias/Channels/Slideshow.int.test.js
@@ -11,30 +11,31 @@ describe('Integration tests - Slideshow', () => {
     let store;
     let dispatchSpy;
 
+    const initState = {
+        imageDisplayed: null,
+        isFullscreenActivated: false,
+        isSlideshowReady: false,
+        slideshow: [
+            { img: "https://nusid.net/slide1.jpg", endTime: 1.0 },
+            { img: "https://nusid.net/slide2.jpg", endTime: 8.0 },
+            { img: "https://nusid.net/slide3.jpg", endTime: 12.0 },
+            { img: "https://nusid.net/slide4.jpg", endTime: 16.0 },
+            { img: "https://nusid.net/slide5.jpg", endTime: 20.0 },
+            { img: "https://nusid.net/slide6.jpg", endTime: 24.0 },
+            { img: "https://nusid.net/slide7.jpg", endTime: 28.0 }
+        ],
+        duration: 28,
+        currentTime: 0,
+        isFullScreenActivated: false,
+        width: 500,
+        height: 315,
+    };
+
     beforeEach(() => {
         ({ store, dispatchSpy } = createTestStore());
     });
 
     it('Slideshow - load', () => {
-        const initState = {
-            imageDisplayed: null,
-            isFullscreenActivated: false,
-            isSlideshowReady: false,
-            slideshow: [
-                { img: "https://nusid.net/slide1.jpg", endTime: 1.0 },
-                { img: "https://nusid.net/slide2.jpg", endTime: 8.0 },
-                { img: "https://nusid.net/slide3.jpg", endTime: 12.0 },
-                { img: "https://nusid.net/slide4.jpg", endTime: 16.0 },
-                { img: "https://nusid.net/slide5.jpg", endTime: 20.0 },
-                { img: "https://nusid.net/slide6.jpg", endTime: 24.0 },
-                { img: "https://nusid.net/slide7.jpg", endTime: 28.0 }
-            ],
-            duration: 28,
-            currentTime: 0,
-            isFullScreenActivated: false,
-            width: 500,
-            height: 315,
-        };
         store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
 
         const slideshowProvider = mount(
@@ -45,9 +46,9 @@ describe('Integration tests - Slideshow', () => {
         const slideshowTrack = slideshowProvider.find("Slideshow");
         const slideshowTrackInstance = slideshowTrack.instance();
         slideshowTrackInstance.load(0);
-        expect(dispatchSpy).toHaveBeenCalledWith({ 
-            type: 'ADD_IMAGE', 
-            payload: { index: expect.anything(), image: expect.anything() } 
+        expect(dispatchSpy).toHaveBeenCalledWith({
+            type: 'ADD_IMAGE',
+            payload: { index: expect.anything(), image: expect.anything() }
         });
         jest.clearAllMocks();
 
@@ -62,25 +63,6 @@ describe('Integration tests - Slideshow', () => {
     });
 
     it('Slideshow - hasEnoughBuffered', () => {
-        const initState = {
-            imageDisplayed: null,
-            isFullscreenActivated: false,
-            isSlideshowReady: false,
-            slideshow: [
-                { img: "https://nusid.net/slide1.jpg", endTime: 1.0 },
-                { img: "https://nusid.net/slide2.jpg", endTime: 8.0 },
-                { img: "https://nusid.net/slide3.jpg", endTime: 12.0 },
-                { img: "https://nusid.net/slide4.jpg", endTime: 16.0 },
-                { img: "https://nusid.net/slide5.jpg", endTime: 20.0 },
-                { img: "https://nusid.net/slide6.jpg", endTime: 24.0 },
-                { img: "https://nusid.net/slide7.jpg", endTime: 28.0 }
-            ],
-            duration: 28,
-            currentTime: 0,
-            isFullScreenActivated: false,
-            width: 500,
-            height: 315,
-        };
         store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
 
         const slideshowProvider = mount(
@@ -97,7 +79,7 @@ describe('Integration tests - Slideshow', () => {
         expect(slideshowTrackInstance.hasEnoughBuffered(0)).toBeFalsy();
         expect(slideshowTrackInstance.hasEnoughBuffered(2)).toBeFalsy();
         expect(slideshowTrackInstance.hasEnoughBuffered(29)).toBeFalsy();
-        
+
         store.getState().slideshow[1].element.dispatchEvent(e);
         store.getState().slideshow[2].element.dispatchEvent(e);
         store.getState().slideshow[4].element.dispatchEvent(e);
@@ -109,25 +91,6 @@ describe('Integration tests - Slideshow', () => {
     });
 
     it('Slideshow - changeTime', () => {
-        const initState = {
-            imageDisplayed: null,
-            isFullscreenActivated: false,
-            isSlideshowReady: false,
-            slideshow: [
-                { img: "https://nusid.net/slide1.jpg", endTime: 1.0 },
-                { img: "https://nusid.net/slide2.jpg", endTime: 8.0 },
-                { img: "https://nusid.net/slide3.jpg", endTime: 12.0 },
-                { img: "https://nusid.net/slide4.jpg", endTime: 16.0 },
-                { img: "https://nusid.net/slide5.jpg", endTime: 20.0 },
-                { img: "https://nusid.net/slide6.jpg", endTime: 24.0 },
-                { img: "https://nusid.net/slide7.jpg", endTime: 28.0 }
-            ],
-            duration: 28,
-            currentTime: 0,
-            isFullScreenActivated: false,
-            width: 500,
-            height: 315,
-        };
         store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
 
         const slideshowProvider = mount(
@@ -143,7 +106,7 @@ describe('Integration tests - Slideshow', () => {
         store.getState().slideshow[0].element.dispatchEvent(e);
         slideshowTrackInstance.changeTime(24);
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SLIDESHOW_IS_NOT_READY' });
-        
+
         store.getState().slideshow[1].element.dispatchEvent(e);
         store.getState().slideshow[2].element.dispatchEvent(e);
         store.getState().slideshow[3].element.dispatchEvent(e);
@@ -155,25 +118,6 @@ describe('Integration tests - Slideshow', () => {
     });
 
     it('Slideshow - play', () => {
-        const initState = {
-            imageDisplayed: null,
-            isFullscreenActivated: false,
-            isSlideshowReady: false,
-            slideshow: [
-                { img: "https://nusid.net/slide1.jpg", endTime: 1.0 },
-                { img: "https://nusid.net/slide2.jpg", endTime: 8.0 },
-                { img: "https://nusid.net/slide3.jpg", endTime: 12.0 },
-                { img: "https://nusid.net/slide4.jpg", endTime: 16.0 },
-                { img: "https://nusid.net/slide5.jpg", endTime: 20.0 },
-                { img: "https://nusid.net/slide6.jpg", endTime: 24.0 },
-                { img: "https://nusid.net/slide7.jpg", endTime: 28.0 }
-            ],
-            duration: 28,
-            currentTime: 0,
-            isFullScreenActivated: false,
-            width: 500,
-            height: 315,
-        };
         store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
 
         const slideshowProvider = mount(
@@ -191,7 +135,7 @@ describe('Integration tests - Slideshow', () => {
         slideshowTrackInstance.pause();
         slideshowTrackInstance.play();
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SLIDESHOW_IS_NOT_READY' });
-        
+
         store.getState().slideshow[1].element.dispatchEvent(e);
         store.getState().slideshow[2].element.dispatchEvent(e);
         store.getState().slideshow[3].element.dispatchEvent(e);
@@ -204,26 +148,29 @@ describe('Integration tests - Slideshow', () => {
         expect(dispatchSpy).toHaveBeenCalledWith({ type: 'SLIDESHOW_IS_READY' });
     });
 
+    it('Slideshow - addPortionBuffered', () => {
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        const slideshowProvider = mount(
+            <Provider store={store}>
+                <Slideshow />
+            </Provider>
+        );
+        const slideshowTrack = slideshowProvider.find("Slideshow");
+        const slideshowTrackInstance = slideshowTrack.instance();
+        slideshowTrackInstance.addPortionBuffered(10, 14);
+        expect(slideshowTrackInstance.timeRangeBuffered(10)).toBe(14);
+        slideshowTrackInstance.addPortionBuffered(8, 14);
+        expect(slideshowTrackInstance.timeRangeBuffered(8)).toBe(14);
+        slideshowTrackInstance.addPortionBuffered(15, 20);
+        expect(slideshowTrackInstance.timeRangeBuffered(8)).toBe(14);
+        slideshowTrackInstance.addPortionBuffered(8, 20);
+        expect(slideshowTrackInstance.timeRangeBuffered(8)).toBe(20);
+
+    });
+
     it('Slideshow - timeRangeBuffered', () => {
-        const initState = {
-            imageDisplayed: null,
-            isFullscreenActivated: false,
-            isSlideshowReady: false,
-            slideshow: [
-                { img: "https://nusid.net/slide1.jpg", endTime: 1.0 },
-                { img: "https://nusid.net/slide2.jpg", endTime: 8.0 },
-                { img: "https://nusid.net/slide3.jpg", endTime: 12.0 },
-                { img: "https://nusid.net/slide4.jpg", endTime: 16.0 },
-                { img: "https://nusid.net/slide5.jpg", endTime: 20.0 },
-                { img: "https://nusid.net/slide6.jpg", endTime: 24.0 },
-                { img: "https://nusid.net/slide7.jpg", endTime: 28.0 }
-            ],
-            duration: 28,
-            currentTime: 0,
-            isFullScreenActivated: false,
-            width: 500,
-            height: 315,
-        };
         store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
 
         const slideshowProvider = mount(
@@ -237,19 +184,81 @@ describe('Integration tests - Slideshow', () => {
 
         const e = new Event('load');
         store.getState().slideshow[0].element.dispatchEvent(e);
-        expect(slideshowTrackInstance.changeTime(0)).toBe(1);
-        expect(slideshowTrackInstance.changeTime(2)).toBe(2);
-        expect(slideshowTrackInstance.changeTime(29)).toBe(29);
-        
+        expect(slideshowTrackInstance.timeRangeBuffered(0)).toBe(1);
+        expect(slideshowTrackInstance.timeRangeBuffered(2)).toBe(2);
+        expect(slideshowTrackInstance.timeRangeBuffered(29)).toBe(29);
+
         store.getState().slideshow[1].element.dispatchEvent(e);
         store.getState().slideshow[2].element.dispatchEvent(e);
         store.getState().slideshow[4].element.dispatchEvent(e);
         store.getState().slideshow[5].element.dispatchEvent(e);
         store.getState().slideshow[6].element.dispatchEvent(e);
-        expect(slideshowTrackInstance.changeTime(0)).toBe(12);
-        expect(slideshowTrackInstance.changeTime(2)).toBe(12);
-        expect(slideshowTrackInstance.changeTime(16)).toBe(28);
-        expect(slideshowTrackInstance.changeTime(29)).toBe(29);
+        expect(slideshowTrackInstance.timeRangeBuffered(0)).toBe(12);
+        expect(slideshowTrackInstance.timeRangeBuffered(2)).toBe(12);
+        expect(slideshowTrackInstance.timeRangeBuffered(16)).toBe(28);
+        expect(slideshowTrackInstance.timeRangeBuffered(29)).toBe(29);
+    });
+
+    it('Slideshow - updateView', () => {
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        const slideshowProvider = mount(
+            <Provider store={store}>
+                <Slideshow />
+            </Provider>
+        );
+        const slideshowTrack = slideshowProvider.find("Slideshow");
+        const slideshowTrackInstance = slideshowTrack.instance();
+        slideshowTrackInstance.load(0);
+
+        const e = new Event('load');
+        store.getState().slideshow[0].element.dispatchEvent(e);
+        store.getState().slideshow[1].element.dispatchEvent(e);
+        store.getState().slideshow[2].element.dispatchEvent(e);
+        store.getState().slideshow[3].element.dispatchEvent(e);
+        store.getState().slideshow[4].element.dispatchEvent(e);
+        store.getState().slideshow[5].element.dispatchEvent(e);
+        store.getState().slideshow[6].element.dispatchEvent(e);
+        jest.clearAllMocks();
+
+        slideshowTrackInstance.currentTime = 0;
+        slideshowTrackInstance.updateView();
+        expect(dispatchSpy).toHaveBeenCalledWith({
+            type: 'UPDATE_IMAGE_DISPLAYED',
+            payload: { 
+                imageDisplayed: {
+                    element: expect.anything(),
+                    endTime: 1, 
+                    img: "https://nusid.net/slide1.jpg"
+                }
+            }
+        });
+
+        slideshowTrackInstance.currentTime = 10;
+        slideshowTrackInstance.updateView();
+        expect(dispatchSpy).toHaveBeenCalledWith({
+            type: 'UPDATE_IMAGE_DISPLAYED',
+            payload: { 
+                imageDisplayed: {
+                    element: expect.anything(),
+                    endTime: 12, 
+                    img: "https://nusid.net/slide3.jpg"
+                }
+            }
+        });
+
+        slideshowTrackInstance.currentTime = 27;
+        slideshowTrackInstance.updateView();
+        expect(dispatchSpy).toHaveBeenCalledWith({
+            type: 'UPDATE_IMAGE_DISPLAYED',
+            payload: { 
+                imageDisplayed: {
+                    element: expect.anything(),
+                    endTime: 28, 
+                    img: "https://nusid.net/slide7.jpg"
+                }
+            }
+        });
     });
 });
 
