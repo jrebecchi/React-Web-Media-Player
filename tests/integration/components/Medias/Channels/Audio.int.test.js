90258d434b8757fe6d4a5512de431277105d300e
JohannC <johann.corcuff@gmail.com>
2019-06-23 11:11:55 +0200

    Test - Audio & Video new integration tests

--- tests/integration/components/Medias/Channels/Audio.int.test.js
+++ tests/integration/components/Medias/Channels/Audio.int.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import createTestStore from '../../../Utils';
 import Adapter from 'enzyme-adapter-react-16';
+import { isIE } from '../../../../../src/services/Utils';
 import Audio from '../../../../../src/components/Medias/Channels/Audio';
 import { Provider } from 'react-redux';
 Enzyme.configure({ adapter: new Adapter() });
@@ -23,7 +24,8 @@ describe('Integration tests - Audio', () => {
     });
 
     it('Audio - timeRangeBuffered', () => {
-        const initState  = {
+
+        const initState = {
             duration: 120,
             audio: "audio-link",
             hasVinyl: true,
@@ -40,10 +42,10 @@ describe('Integration tests - Audio', () => {
         const audioTrackInstance = audioTrack.instance();
         audioTrackInstance.audio = {
             buffered: {
-                0:{start:0, end:5},
-                1:{start:30, end:40},
-                2:{start:60, end:111},
-                3:{start:115, end:120},
+                0: { start: 0, end: 5 },
+                1: { start: 30, end: 40 },
+                2: { start: 60, end: 111 },
+                3: { start: 115, end: 120 },
                 length: 4,
                 start: (i) => audioTrackInstance.audio.buffered[i].start,
                 end: (i) => audioTrackInstance.audio.buffered[i].end
@@ -59,4 +61,126 @@ describe('Integration tests - Audio', () => {
         expect(audioTrackInstance.timeRangeBuffered(130)).toBe(130);
 
     });
+
+    it('Audio - events - Internet Explorer', () => {
+        const initState = {
+            duration: 0,
+            audio: "audio-link",
+            hasVinyl: true,
+            muted: false
+        };
+
+        navigator.__defineGetter__('userAgent', function () {
+            return 'Trident MSIE' // customized user agent
+        });
+
+        expect(isIE()).toBeTruthy()
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        const audioProvider = mount(
+            <Provider store={store}>
+                <Audio />
+            </Provider>
+        );
+        const audioTrack = audioProvider.find("Audio");
+        const audioTrackInstance = audioTrack.instance();
+        audioTrackInstance.audio = {
+            duration: 120
+        }
+
+        audioTrack.simulate("waiting");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("canplaythrough");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("seeking");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("seeked");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("play");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("loadedmetadata");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_DURATION', payload: { duration: 120 } });
+        jest.clearAllMocks();
+    });
+
+    it('Audio - events - Chrome/Firefox/Safari...', () => {
+        const initState = {
+            duration: 0,
+            audio: "audio-link",
+            hasVinyl: true,
+            muted: false
+        };
+
+        navigator.__defineGetter__('userAgent', function () {
+            return 'Mozilla/5.0 (win32) AppleWebKit/537.36 (KHTML, like Gecko) jsdom/11.12.0'
+        });
+
+        expect(isIE()).toBeFalsy()
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        const audioProvider = mount(
+            <Provider store={store}>
+                <Audio />
+            </Provider>
+        );
+        const audioTrack = audioProvider.find("Audio");
+        const audioTrackInstance = audioTrack.instance();
+        audioTrackInstance.audio = {
+            duration: 120,
+            currentTime: 100
+        }
+
+        audioTrack.simulate("waiting");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("canplaythrough");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("seeking");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("seeked");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("play");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        audioTrack.simulate("loadedmetadata");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_DURATION', payload: { duration: 120 } });
+        jest.clearAllMocks();
+
+        audioTrackInstance.audio = {
+            duration: 120,
+            currentTime: 120
+        }
+
+        audioTrack.simulate("waiting");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        initState.hasVinyl = false;
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        audioTrack.simulate("loadedmetadata");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'UPDATE_DURATION', payload: { duration: 120 } });
+        jest.clearAllMocks();
+    });
 });
+
--- tests/integration/components/Medias/Channels/Video.int.test.js
+++ tests/integration/components/Medias/Channels/Video.int.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import createTestStore from '../../../Utils';
 import Adapter from 'enzyme-adapter-react-16';
+import { isIE } from '../../../../../src/services/Utils';
 import Video from '../../../../../src/components/Medias/Channels/Video';
 import { Provider } from 'react-redux';
 Enzyme.configure({ adapter: new Adapter() });
@@ -57,6 +58,141 @@ describe('Integration tests - Video', () => {
         expect(videoTrackInstance.timeRangeBuffered(115)).toBe(120);
         expect(videoTrackInstance.timeRangeBuffered(120)).toBe(120);
         expect(videoTrackInstance.timeRangeBuffered(130)).toBe(130);
+    });
 
+    it('Video - events - Internet Explorer', () => {
+        const initState = {
+            duration: 0,
+            video: "video-link",
+            muted: false
+        };
+
+        navigator.__defineGetter__('userAgent', function(){
+            return 'Trident MSIE' // customized user agent
+        });
+
+        expect(isIE()).toBeTruthy()
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        const videoProvider = mount(
+            <Provider store={store}>
+                <Video />
+            </Provider>
+        );
+        const videoTrack = videoProvider.find("Video");
+        const videoTrackInstance = videoTrack.instance();
+        videoTrackInstance.video = {
+            duration: 120,
+            videoWidth: 560,
+            videoHeight:315,
+            currentTime:80
+        }
+        expect(videoTrackInstance.getDuration()).toBe(120);
+
+        videoTrack.simulate("waiting");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'VIDEO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("canplaythrough");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'VIDEO_IS_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("loadedmetadata");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_DURATION', payload: { duration: 120 } });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_VIDEO_WIDTH', payload: { videoWidth: 560 } });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_VIDEO_HEIGHT', payload: { videoHeight: 315 } });
+        jest.clearAllMocks();
+
+        /*videoTrack.simulate("seeking");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'VIDEO_IS_NOT_READY' });
+        jest.clearAllMocks();*/
+
+        videoTrack.simulate("seeked");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'VIDEO_IS_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("play");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'VIDEO_IS_READY' });
+        jest.clearAllMocks();
+
+        videoTrackInstance.video = {
+            duration: 120,
+            videoWidth: 560,
+            videoHeight: 315,
+            currentTime: 120
+        }
+
+        videoTrack.simulate("seeking");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'VIDEO_IS_NOT_READY' });
+        jest.clearAllMocks();
+    });
+
+    it('Video - events - Chrome/Firefox/Safari...', () => {
+        const initState = {
+            duration: 0,
+            audio: "audio-link",
+            hasVinyl: true,
+            muted: false
+        };
+
+        navigator.__defineGetter__('userAgent', function(){
+            return 'Mozilla/5.0 (win32) AppleWebKit/537.36 (KHTML, like Gecko) jsdom/11.12.0'
+        });
+
+        expect(isIE()).toBeFalsy()
+
+        store.dispatch({ type: "INIT_STATE", payload: { state: initState } });
+
+        const videoProvider = mount(
+            <Provider store={store}>
+                <Video />
+            </Provider>
+        );
+        const videoTrack = videoProvider.find("Video");
+        const videoTrackInstance = videoTrack.instance();
+        videoTrackInstance.video = {
+            duration: 120,
+            videoWidth: 560,
+            videoHeight:315,
+            currentTime:80
+        }
+        expect(videoTrackInstance.getDuration()).toBe(120);
+
+        videoTrack.simulate("waiting");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'VIDEO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("canplaythrough");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'VIDEO_IS_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("loadedmetadata");
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_DURATION', payload: { duration: 120 } });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_VIDEO_WIDTH', payload: { videoWidth: 560 } });
+        expect(dispatchSpy).toHaveBeenCalledWith({ type: 'UPDATE_VIDEO_HEIGHT', payload: { videoHeight: 315 } });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("seeking");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("seeked");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+        videoTrack.simulate("play");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_READY' });
+        jest.clearAllMocks();
+
+
+        videoTrackInstance.audio = {
+            duration: 120,
+            currentTime:120
+        }
+
+        videoTrack.simulate("waiting");
+        expect(dispatchSpy).not.toHaveBeenCalledWith({ type: 'AUDIO_IS_NOT_READY' });
+        jest.clearAllMocks();
     });
 });
